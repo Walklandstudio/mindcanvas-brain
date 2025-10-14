22db28e6d6de1366d6bf81ac1fdec99fc1e53782
Walklandstudio <lisa@studiods.co.za>
2025-10-14 12:32:56 +0000

    Commit

--- app/t/[token]/done/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DonePage() {
-  return (
-    <main className="mx-auto max-w-xl p-8 space-y-4">
-      <h1 className="text-2xl font-semibold">Thanks!</h1>
-      <p>Your responses have been submitted.</p>
-    </main>
-  );
-}
