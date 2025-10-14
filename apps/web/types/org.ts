323181eff8cdd78dba06a4e27dcd9dce2c1485e3
Walklandstudio <lisa@studiods.co.za>
2025-10-14 14:50:51 +0000

    commit

--- apps/web/types/org.ts
+++
@@ -1,9 +0,0 @@
-// apps/web/types/org.ts
-export type OrgProfile = {
-  id: string;
-  org_id: string;
-  framework_id: string;
-  name: string;
-  frequency: 'A'|'B'|'C'|'D';
-  ordinal: number;
-};
