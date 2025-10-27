c91a987f7cc978bfb07cf9ed1c9a9bda2c96a262
Walklandstudio <lisa@studiods.co.za>
2025-10-27 12:20:21 +0000

    commit 15

---
+++ apps/web/lib/supabaseAdmin.ts
@@ -0,0 +1,6 @@
+// lib/supabaseAdmin.ts
+import { createClient } from '@supabase/supabase-js';
+export const sbAdmin = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE!,
+  { auth: { persistSession: false } }
+);
