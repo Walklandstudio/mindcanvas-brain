769e6525a01bdb3e32c4bc159ed865a365584c29
Walklandstudio <lisa@studiods.co.za>
2025-10-27 13:42:47 +0000

    commit 25

--- apps/web/app/api/ping/route.ts
+++
@@ -1,7 +0,0 @@
-export const runtime = 'nodejs';
-export const dynamic = 'force-dynamic';
-export async function GET() {
-  const hasUrl = !!process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const hasRole = !!process.env.SUPABASE_SERVICE_ROLE;
-  return Response.json({ ok: hasUrl && hasRole, hasUrl, hasRole });
-}
